78787ef60ffaa84bce950ce8b4783995d3adcae2
Simon Fridlund <simon.r.k.fridlund@gmail.com>
2015-02-05 10:43:41 +0100

    Version bump

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   git: 'https://github.com/zimme/meteor-collection-behaviours.git',
   name: 'zimme:collection-behaviours',
   summary: 'Define and attach behaviours on collections',
-  version: '1.0.2'
+  version: '1.0.3'
 });
 
 Package.onUse(function(api) {
